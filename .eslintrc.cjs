48f8d83dbdc644e368c5ea719690c9ad3f208a29
zeegglar <ao.azeezat@gmail.com>
2025-09-14 12:44:03 -0500

    feat: Update ESLint rules and TypeScript configuration for improved linting and flexibility

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,9 @@ module.exports = {
     ],
     'no-unused-vars': 'off', // Disable for now to avoid conflicts
     'react/prop-types': 'off', // We use TypeScript for prop validation
+    'react/no-unescaped-entities': 'off', // Allow unescaped quotes in JSX
+    'react-hooks/exhaustive-deps': 'warn', // Warn instead of error
+    'no-case-declarations': 'off', // Allow variable declarations in case blocks
     'prefer-const': 'error',
     'no-var': 'error',
   },
