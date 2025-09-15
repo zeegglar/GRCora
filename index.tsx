9c7c59cf68a532b1ef613fa14744893bc00f5874
zeegglar <ao.azeezat@gmail.com>
2025-09-15 15:53:09 -0500

    Remove importmap script from index.html and replace SimpleApp with TestApp in index.tsx; add new debug.html for testing JavaScript functionality and React module loading

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SimpleApp from './SimpleApp';
+import TestApp from './TestApp';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -11,7 +11,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <SimpleApp />
+    <TestApp />
   </React.StrictMode>
 );
 
